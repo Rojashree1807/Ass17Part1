183f6cef88872276b9f5553173ea8b086b28f7ef
Rojashree1807 <rojarojashree1807@gmail.com>
2023-11-21 15:15:23 +0530

    first commit

---
+++ ConAppAss17Part1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAss17Part1
+{
+   
+    class Program
+    {
+        static void Main()
+        {
+            BankAccount myAccount = new BankAccount("Rojashree");
+
+            Console.WriteLine($"Account Number: {myAccount.AccountNumber}");
+            Console.WriteLine($"Account Holder: {myAccount.AccountHolderName}");
+            Console.WriteLine($"Initial Balance: ${myAccount.Balance}");
+
+            myAccount.Deposit(1000);
+            myAccount.Withdraw(500);
+
+            Console.WriteLine($"Final Balance: ${myAccount.Balance}");
+            Console.ReadKey();
+        }
+    }
+
+} 
